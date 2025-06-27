7514276be262aaa63d4158b5f6bd51eed800343e
Wallace Queiros <wallacequeiros@hotmail.com>
2025-06-27 10:20:52 -0300

    docs(model)adicionar comentários explicativos na classe Nokia e Iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,12 +1,16 @@
 namespace DesafioPOO.Models
 {
+    // Classe Iphone herda da classe abstrata Smartphone
     public class Iphone : Smartphone
     {
+        // Construtor da classe Iphone que repassa os parâmetros para a classe base (‘Smartphone’)
         public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
             
         }
 
+        // Implementação obrigatória do método abstrato da classe base
+        // Define qual o aplicativo será "instalado" no ‘smartphone’ Iphone
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando Aplicativo {nomeApp} no Smartphone Iphone");
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,12 +1,16 @@
 namespace DesafioPOO.Models
 {
+    // Classe Nokia herda da classe abstrata Smartphone
     public class Nokia : Smartphone
     {
+        // Construtor da classe Nokia que repassa os parâmetros para a classe base (‘Smartphone’)
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
             
         }
         
+        // Implementação obrigatória do método abstrato da classe base
+        // Define qual o aplicativo será "instalado" no ‘smartphone’ Nokia
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando Aplicativo {nomeApp} no Smartphone Nokia");
